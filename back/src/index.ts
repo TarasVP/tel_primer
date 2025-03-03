8a5f354b390dfd0721cbd37320d829f5cba20636
taras <taras_vp@mail.ru>
2025-03-04 01:38:18 +0300

    add trpc

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,10 +1,21 @@
 import express from "express";
+import * as trpcExpress from '@trpc/server/adapters/express'
+import { trpcRouter } from './trpc'
+
+
 
 const expressApp = express();
 
 expressApp.get("/ping", (req, res) => {
   res.send("pong");
 });
+
+expressApp.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: trpcRouter,
+  })
+)
 
 expressApp.listen(3000, () => {
   console.info("Listen ah http://localhost:3000");
---
+++ back/src/trpc.ts
@@ -0,0 +1,19 @@
+import { initTRPC } from '@trpc/server'
+
+const categories = [
+  { "id": "1", name: "Наука"},
+  { "id": "2", name: "Спорт"},
+  { "id": "3", name: "Политика"},
+  { "id": "4", name: "Искусство"},
+  { "id": "5", name: "Разное"},
+]
+
+const trpc = initTRPC.create()
+
+export const trpcRouter = trpc.router({
+  getIdeas: trpc.procedure.query(() => {
+    return { categories }
+  }),
+})
+
+export type TrpcRouter = typeof trpcRouter
