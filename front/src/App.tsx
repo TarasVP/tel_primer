93bdfb8f58f2cd7995dadb96008a4b64743c705f
taras <taras_vp@mail.ru>
2025-03-09 14:41:03 +0300

    Routers

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { TrpcProvider } from './lib/trps'
 import { CategoriesPage } from './pages/CategoriesPage'
+import { SubCategoriesPage } from './pages/SubCategoriesPage'
 
 export const App = () => {
   return (
     <TrpcProvider>
-      <CategoriesPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<CategoriesPage />} />
+          <Route path="/categories/:categoryId" element={<SubCategoriesPage />} />
+        </Routes>
+      </BrowserRouter>
     </TrpcProvider>
   )
 }
---
+++ front/src/pages/SubCategoriesPage.tsx
@@ -0,0 +1,12 @@
+export const SubCategoriesPage = () => {
+  return (
+    <div>
+      <h1>Category</h1>
+      <div>
+        <p>subcategory 1</p>
+        <p>subcategory 2</p>
+        <p>subcategory 3</p>
+      </div>
+    </div>
+  )
+}
