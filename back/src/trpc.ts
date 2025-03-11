e0f4eb5d71cc04beeab0a8e63c875c4d10ca01ca
Taras <taras_vp@mail.ru>
2025-03-11 19:02:35 +0300

    Lodash

--- back/src/trpc.ts
+++ back/src/trpc.ts
@@ -1,18 +1,25 @@
 import { initTRPC } from '@trpc/server'
+import _ from 'lodash'
 
-const categories = [
+/* const categories = [
   { id: '1', name: 'Наука' },
   { id: '2', name: 'Спорт' },
   { id: '3', name: 'Политика' },
   { id: '4', name: 'Искусство' },
   { id: '5', name: 'Разное' },
-]
+] */
+
+const categories = _.times(100, (i) => ({
+  id: `${i}`,
+  name: `category ${i}`,
+  text: _.times(100, (j) => `<p>Text paragraph ${j}...</p>`).join(''),
+}))
 
 const trpc = initTRPC.create()
 
 export const trpcRouter = trpc.router({
   getCategories: trpc.procedure.query(() => {
-    return { categories }
+    return { categories: categories.map((category) => _.pick(category, ['id', 'name'])) }
   }),
 })
 
