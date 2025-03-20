9e5ece3b02707c5f92c305f6a04d7340aa341f57
Taras <taras_vp@mail.ru>
2025-03-20 18:13:06 +0300

    Add generate index extention comments

--- back/src/router/index.ts
+++ back/src/router/index.ts
@@ -1,10 +1,14 @@
 import { trpc } from '../lib/trpc'
+// @index('./**/index.ts', f => `import { ${f.path.split('/').slice(0, -1).pop()}TrpcRoute } from '${f.path.split('/').slice(0, -1).join('/')}'`)
 import { getCategoriesTrpcRoute } from './getCategories'
 import { getSubCategoriesTrpcRoute } from './getSubCategories'
+// @endindex
 
 export const trpcRouter = trpc.router({
+  // @index('./**/index.ts', f => `${f.path.split('/').slice(0, -1).pop()}: ${f.path.split('/').slice(0, -1).pop()}TrpcRoute,`)
   getCategories: getCategoriesTrpcRoute,
   getSubCategories: getSubCategoriesTrpcRoute,
+  // @endindex
 })
 
 export type TrpcRouter = typeof trpcRouter
