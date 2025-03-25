72f756cd6a860afc4efcb7236011045cec707745
taras <taras_vp@mail.ru>
2025-03-25 17:27:05 +0300

    Add prisma scripts

---
+++ back/src/prisma/migrations/20250325140559_add_telegrino_model/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
