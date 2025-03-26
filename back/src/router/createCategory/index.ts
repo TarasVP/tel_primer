8d363b49dc6d0316be0e87b199e8ebc8a8624910
taras <taras_vp@mail.ru>
2025-03-26 18:30:03 +0300

    Prisma create cdtegory

--- back/src/router/createCategory/index.ts
+++ back/src/router/createCategory/index.ts
@@ -1,11 +1,19 @@
-import { categories } from '../../lib/categories'
 import { trpc } from '../../lib/trpc'
 import { zCreateCategoryTrpcInput } from './input'
 
-export const createCategoryTrpcRoute = trpc.procedure.input(zCreateCategoryTrpcInput).mutation(({ input }) => {
-  if (categories.find((category) => category.id === input.id)) {
-    throw Error('Category with this id already exists')
-  }
-  categories.unshift(input)
-  return true
-})
+export const createCategoryTrpcRoute = trpc.procedure
+  .input(zCreateCategoryTrpcInput)
+  .mutation(async ({ input, ctx }) => {
+    const exIdea = await ctx.prisma.category.findUnique({
+      where: {
+        id: input.id,
+      },
+    })
+    if (exIdea) {
+      throw Error('Idea with this nick already exists')
+    }
+    await ctx.prisma.category.create({
+      data: input,
+    })
+    return true
+  })
