38a81d5f701fd82efdda38de0ed65cd01d031292
taras <taras_vp@mail.ru>
2025-03-27 15:40:47 +0300

    Add trpc-playground

--- back/src/lib/trpc.ts
+++ back/src/lib/trpc.ts
@@ -4,16 +4,29 @@ import { type Express } from 'express'
 import * as trpcExpress from '@trpc/server/adapters/express'
 import { AppContext } from './ctx'
 import superjson from 'superjson'
+import { expressHandler } from 'trpc-playground/handlers/express'
 
 export const trpc = initTRPC.context<AppContext>().create({
   transformer: superjson,
 })
-export const applyTrpcToExpressApp = (expressApp: Express, appContext: AppContext, trpcRouter: TrpcRouter) => {
+export const applyTrpcToExpressApp = async (expressApp: Express, appContext: AppContext, trpcRouter: TrpcRouter) => {
   expressApp.use(
     '/trpc',
     trpcExpress.createExpressMiddleware({
       router: trpcRouter,
       createContext: () => appContext,
+    })
+  )
+
+  expressApp.use(
+    '/trpc-playground',
+    await expressHandler({
+      trpcApiEndpoint: '/trpc',
+      playgroundEndpoint: '/trpc-playground',
+      router: trpcRouter,
+      request: {
+        superjson: true,
+      },
     })
   )
 }
