aff5937fd3833fc7b49670726abe6b2142e2f12b
taras <taras_vp@mail.ru>
2025-07-04 19:01:26 +0300

    Tests import prohibition

--- shared/eslint.config.js
+++ shared/eslint.config.js
@@ -10,6 +10,13 @@ export default [
         project: "./tsconfig.json",
       },
     },
+    settings: {
+      "import/resolver": {
+        node: {
+          extensions: [".js", ".ts", ".tsx"],
+        },
+      },
+    },
   },
   {
     rules: {
